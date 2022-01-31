50eae7574db54823e74eb8cd998d828462522b41
Josevaldo10 <mycarima@gmail.com>
2022-01-31 12:57:54 -0300

    Configurados Uploads com Collectfast

--- pypro/settings.py
+++ pypro/settings.py
@@ -31,6 +31,7 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
+AUTH_USER_MODEL = 'base.user'
 
 # Application definition
 
@@ -42,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'collectfast',
     'django.contrib.staticfiles',
-    'pypro.base.migrations',
+    'pypro.base',
 ]
 
 MIDDLEWARE = [

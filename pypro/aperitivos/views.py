87488101be85238efe62549f1783dae0a7c38033
Josevaldo10 <mycarima@gmail.com>
2022-02-21 21:46:41 -0300

    Corrigido Acesso a Método no Template

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -22,3 +22,9 @@ videos_dct = {v.slug: v for v in videos}
 
 def indice(request):
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
+
+
+def video(request, slug):
+    video = videos_dct[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})

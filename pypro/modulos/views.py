7a395d903476904dc271b01839b32f261002a7d9
Josevaldo10 <mycarima@gmail.com>
2022-03-05 21:05:38 -0300

    Feita Adição de Coluna em Tabela

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from pypro.modulos import facade
+
+
+def detalhe(request, slug):
+    modulo = facade.encontrar_modulo(slug)
+    aulas = facade.listar_aulas_de_modulo_ordenadas(modulo)
+    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
+
+
+@login_required
+def aula(request, slug):
+    aula = facade.encontrar_aula(slug)
+    return render(request, 'modulos/aula_detalhe.html', {'aula': aula})

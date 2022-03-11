30e45c24e4dbc08e403af11c563e8991b38b4ccf
Josevaldo10 <mycarima@gmail.com>
2022-03-10 23:37:59 -0300

    Configurdo Modelo com Model Meta

--- pypro/modulos/tests/test_aula_detalhe.py
+++ pypro/modulos/tests/test_aula_detalhe.py
@@ -1,6 +1,6 @@
-from django.urls.base import reverse
 import pytest
-from model_mommy import mommy
+from django.urls.base import reverse
+from model_bakery import baker
 
 from pypro.django_assertions import assert_contains
 from pypro.modulos.models import Modulo, Aula
@@ -8,12 +8,12 @@ from pypro.modulos.models import Modulo, Aula
 
 @pytest.fixture
 def modulo(db):
-    return mommy.make(Modulo)
+    return baker.make(Modulo)
 
 
 @pytest.fixture
 def aula(modulo):
-    return mommy.make(Aula, modulo=modulo)
+    return baker.make(Aula, modulo=modulo)
 
 
 @pytest.fixture
@@ -27,4 +27,14 @@ def test_titulo(resp, aula: Aula):
 
 
 def test_vimeo(resp, aula: Aula):
-    assert_contains(resp, f'src="https://player.vimeo.com/video/{ aula.vimeo_id }"')
+    assert_contains(resp, f'src="https://player.vimeo.com/video/{aula.vimeo_id}"')
+
+
+def test_modulo_breadcrumb(resp, modulo):
+    assert_contains(resp, f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url}">{modulo.titulo}</a></li>')
+
+@pytest.fixture
+def resp_sem_usuario(client, aula):
+    resp = client.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
+    return resp
+
